0569f5ed14a97376375a9845f50d41fcc9040f1f
najaf2 <najafali@ou.edu>
2021-11-15 16:17:41 -0600

    Added serializing and deserializing on all types

--- src/light.rs
+++ src/light.rs
@@ -1,7 +1,10 @@
+use serde::Deserialize;
+use serde::Serialize;
+
 use crate::color::Color;
 use crate::vector2::Vector2;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
 pub struct Light {
     position: Vector2,
     color: Color,
--- src/material.rs
+++ src/material.rs
@@ -1,4 +1,7 @@
-#[derive(Copy, Clone, Debug)]
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
 pub enum Material {
     Grass,
     Rubble,
