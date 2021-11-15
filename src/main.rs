92a202c1aac2fff792f7355ad0f70dd983c33780
Chay Nabors <chaynabors@gmail.com>
2021-11-15 15:03:48 -0600

    Updated logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod color;
 mod vector2;
 
+use std::io::Write;
+
+use chrono::Local;
+use log::LevelFilter;
+use log::info;
 use winit::event::Event;
 use winit::event::WindowEvent;
 use winit::event_loop::ControlFlow;
@@ -8,18 +13,31 @@ use winit::event_loop::EventLoop;
 use winit::window::WindowBuilder;
 
 fn main() {
-    env_logger::init();
+    env_logger::builder()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} {}: {}",
+                record.level(),
+                Local::now().format("%H:%M:%S"),
+                record.args()
+            )
+        })
+        .filter_level(LevelFilter::Info)
+        .init();
 
+    info!("Creating event loop and window");
     let event_loop = EventLoop::new();
     let _window = WindowBuilder::new().build(&event_loop).unwrap();
+    info!("Created event loop and window");
 
+    info!("Entering event loop");
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
 
         match event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::Destroyed => *control_flow = ControlFlow::Exit,
                 _ => (),
             }
             _ => (),
