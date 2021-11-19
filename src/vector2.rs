b21188add3a26542a9dc670682047bea6f114d7a
Chay Nabors <chaynabors@gmail.com>
2021-11-18 18:35:39 -0600

    Cleanup of Vector2 test

--- src/vector2.rs
+++ src/vector2.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 use serde::Serialize;
 
-#[derive(Copy, Clone, Debug, Deserialize, Serialize)]
+#[derive(Copy, Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Vector2 {
     pub x: f32,
     pub y: f32,
@@ -9,16 +9,12 @@ pub struct Vector2 {
 
 #[cfg(test)]
 mod tests {
-    use crate::vector2::Vector2;
+    use super::Vector2;
 
     #[test]
-    fn it_works() {
-        let vector2 = Vector2{ x: 200., y: 200. };
-
-        let serialized = serde_json::to_string_pretty(&vector2).unwrap();
-        println!("serialized = {}", serialized);
-    
-        let deserialized: Vector2 = serde_json::from_str(&serialized).unwrap();
-        println!("deserialized = {:?}", deserialized);
+    fn serialization() {
+        let vector2 = Vector2::default();
+        let serialized = serde_json::to_string(&vector2).unwrap();
+        serde_json::from_str::<Vector2>(&serialized).unwrap();
     }
 }
